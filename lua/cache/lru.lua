84494f3251ac1d62b20a75313fc66a46a149cb20
uga-rosa <uga6603@gmail.com>
2021-12-03 16:40:35 +0900

    lru

---
+++ lua/cache/lru.lua
@@ -0,0 +1,87 @@
+---@class CacheNode
+---@field key integer
+---@field value integer
+---@field prev CacheNode
+---@field next CacheNode
+local CacheNode = setmetatable({}, {
+    __call = function(_, key, value)
+        return {
+            key = key,
+            value = value,
+            prev = nil,
+            next = nil,
+        }
+    end,
+})
+
+---@class LinkedList
+---@field head CacheNode
+---@field tail CacheNode
+local LinkedList = {}
+
+setmetatable(LinkedList, {
+    __call = function()
+        local self = {}
+        self.head = CacheNode(0, 0) -- dummy
+        self.tail = CacheNode(0, 0) -- dummy
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        return setmetatable(self, { __index = LinkedList })
+    end,
+})
+
+function LinkedList:add(node)
+    node.prev = self.head
+    self.head.next = node
+    node.next = self.head.next
+    node.next.prev = node
+end
+
+function LinkedList:remove(node)
+    node.prev.next = node.next
+    node.next.prev = node.prev
+end
+
+function LinkedList:move2head(node)
+    self:remove(node)
+    self:add(node)
+end
+
+---@class LruCache
+---@field capacity integer
+---@field key2node table<integer, CacheNode>
+---@field linked_list LinkedList
+local LruCache = {}
+
+setmetatable(LruCache, {
+    __call = function(_, capacity)
+        local self = {}
+        self.capacity = capacity
+        self.key2node = {}
+        self.linked_list = LinkedList()
+        return setmetatable(self, { __index = LruCache })
+    end,
+})
+
+function LruCache:put(key, value)
+    if self.key2node[key] then
+        self.key2node[key].value = value
+        self.linked_list:move2head(self.key2node[key])
+    else
+        local node = CacheNode(key, value)
+        self.key2node[key] = node
+        self.linked_list:add(node)
+
+        if vim.tbl_count(self.key2node) > self.capacity then
+            self.key2node[self.linked_list.tail.prev.key] = nil
+            self.linked_list:remove(self.linked_list.tail.prev)
+        end
+    end
+end
+
+function LruCache:get(key)
+    if self.key2node[key] then
+        self.linked_list.move2head(self.key2node[key])
+        return self.key2node[key].value
+    end
+end
